66d1385c9655450d91d6ecb19184610e3d697cd1
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2020-03-29 13:42:54 +0530

    Sending req done

--- src/Components/Users/Card.js
+++ src/Components/Users/Card.js
@@ -15,14 +15,49 @@ const Card =(props) => {
 		 		props.superUser[0].leader ?
 		 			props.user.member ?
 				 		<div className='btn'>
-			    	<Button variant="dark " onClick={props.sendReq(props.username)}>Send Request</Button>
+			    	<Button variant="dark " onClick={
+					     (
+					    	()=>{
+					    		fetch('http://localhost:5000/createNotif',{
+							      method:'post',
+							      headers: {'Content-Type':'application/json'},
+							      body: JSON.stringify({
+							        toUsername:props.user.username,
+							        fromGname:props.superUser[0].username
+							      })
+							    }).then(res=>res.json())
+							      .then((res)=>{
+							      	alert('Your req has been sent to ' + props.user.username + '!')
+							      })
+							      .catch(err=>console.log);
+					    	}
+					    )
+			    	}>Send Request</Button>
 			    	</div>	
 		    	: console.log('hi')
 		    :
 		    props.superUser[0].member ?
 		    	props.user.leader ?	
 			    	<div className='btn'>
-			    	<Button variant="dark " onClick={props.sendReq(props.username)}>Send Request</Button>
+			    	<Button variant="dark " onClick={
+					    (
+					    	()=>{
+					    		fetch('http://localhost:5000/createNotif',{
+							      method:'post',
+							      headers: {'Content-Type':'application/json'},
+							      body: JSON.stringify({
+							        fromUsername:props.superUser[0].username,
+							        toGname:props.user.username
+							      })
+							    }).then(res=>res.json())
+							      .then((res)=>{
+							      	alert('Your req has been sent to ' + props.user.username + '!')
+							      })
+							      .catch(err=>console.log);
+					    	}
+					    )
+					    
+			    	}>Send Request</Button>
 			    	</div>	
 		    	: console.log('hi')
 		    : console.log('hi')	
