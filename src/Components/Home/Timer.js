6e9cf1693c497ba87332fc85cbeb3dafb0486457
sristi <emailsristi@gmail.com>
2020-06-13 13:16:35 +0530

    timer

--- src/Components/Home/Timer.js
+++ src/Components/Home/Timer.js
@@ -1,84 +1,45 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import './Timer.css';
 export default class Timer extends Component {
-	constructor() {
-		super();
-		this.state = {
-			hour: '00',
-			sec: '00',
-			min: '',
-		}
-		this.secondsRemaining = ''; 
-		this.intervalHandle = '';
-		this.tick = this.tick.bind(this);
-	}
+
 
 	componentDidMount() {
 		axios.get('http://localhost:5000/timer/5edf6769c0264e6ed03657e9')
 			.then(res => {
 				console.log(res.data);
-				var countdown = new Date(res.data.countdown);
-				// var date = countdown.getDate();
-				var hour = countdown.getHours();
-				var min = countdown.getMinutes();
-				var sec = countdown.getSeconds();
-				var nowhour = new Date().getHours();
-				var nowmin = new Date().getMinutes();
-				var nowsec = new Date().getSeconds();
-				if(sec>nowsec)
-					var setsec = sec-nowsec;
-				else
-					var setsec = 60-nowsec+sec;
-				if(min>nowmin)
-					var setmin = min-nowmin;
-				else	
-					var setmin = 60-nowmin+min;				
-				this.setState({
-					sec: setsec,
-					min: setmin,
-					hour: hour-nowhour,
-				})
-				this.secondsRemaining = this.state.min * 60;
-				this.intervalHandle = setInterval(this.tick, 1000);
-			})
+				// var countdown = new Date(res.data.countdown);
+				// console.log(countdown);			
+				const second = 1000,
+				minute = second * 60,
+				hour = minute * 60,
+				day = hour * 24;		  
+				  let countDown = new Date('June 13, 2020 13:14:10').getTime();//this is where u input the date
+				  console.log(countDown);
+			  	let x = setInterval(function() { 		  
+				let now = new Date().getTime(),
+					distance = countDown - now;		  
+					document.getElementById('days').innerText = Math.floor(distance / (day));
+				  document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour));
+				  document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute));
+				  document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+			  }, second)
+				
+			 })
 	}
-
-	tick=()=>{
-		var min = Math.floor(this.secondsRemaining / 60);
-		var sec = this.secondsRemaining - (min * 60);
-		this.setState({
-			min: min,
-			sec: sec
-		})
-		if (sec < 10) {
-			this.setState({
-				sec: "0" + this.state.seconds,
-			})
-		}
-		if (min < 10) {
-		this.setState({
-			min: "0" + min,
-		})
-		}
-		if (min === 0 & sec === 0) {
-		clearInterval(this.intervalHandle);
-		}
-		this.secondsRemaining--
-	}
-
 	render() {
 		return (
 			<div>
-				<center>
-				<h2>min:{this.state.min} sec: {this.state.sec}</h2>
-				{
-					(()=>{
-						console.log('min',this.state.min);
-						console.log('sec',this.state.sec);
-					})()
-				}
-				</center>
+				<div class="container-timer">
+				<h1 className="timer-heading">Countdown to end of Sprint 1/2</h1>
+				<ul className="unordered-list">
+					<li className="list-item"><span id="days"></span>Seconds</li>
+					<li className="list-item"><span id="hours"></span>Seconds</li>
+					<li className="list-item"><span id="minutes"></span>Minutes</li>
+					<li className="list-item"><span id="seconds"></span>Seconds</li>
+					
+				</ul>
+				</div>
 			</div>
 		);
 	}
