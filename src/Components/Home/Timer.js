e0e89a1a23a5acfc4f6d0b6dcf7df770e42f5348
sristi <emailsristi@gmail.com>
2020-06-13 13:35:26 +0530

    timer 2

--- src/Components/Home/Timer.js
+++ src/Components/Home/Timer.js
@@ -8,13 +8,13 @@ export default class Timer extends Component {
 		axios.get('http://localhost:5000/timer/5edf6769c0264e6ed03657e9')
 			.then(res => {
 				console.log(res.data);
-				// var countdown = new Date(res.data.countdown);
-				// console.log(countdown);			
+				 var countdown = new Date(res.data.countdown);
+				 console.log(countdown);			
 				const second = 1000,
 				minute = second * 60,
 				hour = minute * 60,
 				day = hour * 24;		  
-				  let countDown = new Date('June 13, 2020 13:14:10').getTime();//this is where u input the date
+				  let countDown = new Date(countdown).getTime();//this is where u input the date
 				  console.log(countDown);
 			  	let x = setInterval(function() { 		  
 				let now = new Date().getTime(),
@@ -33,8 +33,8 @@ export default class Timer extends Component {
 				<div class="container-timer">
 				<h1 className="timer-heading">Countdown to end of Sprint 1/2</h1>
 				<ul className="unordered-list">
-					<li className="list-item"><span id="days"></span>Seconds</li>
-					<li className="list-item"><span id="hours"></span>Seconds</li>
+					<li className="list-item"><span id="days"></span>Days</li>
+					<li className="list-item"><span id="hours"></span>Hours</li>
 					<li className="list-item"><span id="minutes"></span>Minutes</li>
 					<li className="list-item"><span id="seconds"></span>Seconds</li>
 					
