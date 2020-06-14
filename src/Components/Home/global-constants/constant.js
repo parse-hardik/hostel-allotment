d2f0e378cce8ad6414d33925e84c629d5d58d23f
sristi <emailsristi@gmail.com>
2020-06-14 21:01:06 +0530

    guidelines

---
+++ src/Components/Home/global-constants/constant.js
@@ -0,0 +1,77 @@
+ export const FAQS = [
+  {
+    Category:'General',
+    Ques: ' How does Zhiffy work? ',
+    Ans:'Check this <b>here</b>  ',
+  },
+  {
+    Category:'General',
+    Ques: ' What is Zhiffy? ',
+    Ans:"Zhiffy is an E-Commerce platform for exchanging your electronics quickly. You can sell the electronics you don't need and choose what you'd like to get in exchange. Finding, Best Price, Quality Guarantee & Delivery - we'll take care of the rest",
+  },
+  {
+    Category:'General',
+    Ques: ' What is a Recommerced item?  ',
+    Ans:" A ReCommerced item is an item coming from the user or manufacturer also called Repeat is considered to be one of the below 4 categories:",
+  },
+  {
+    Category:'General',
+    Ques: 'Do you charge anything?',
+    Ans:"We don't charge anything from the customer. We take a sales commission from them for leading the sale to them. We ONLY take a sales commission. We will NEVER over quote the item the vendor is selling i.e. we never make a profit by charging extra on the item you get in exchange. We directly share the price they are charging.",
+  },
+  {
+    Category:'General',
+    Ques: 'Is my personal information in the devices safe with Zhiffy?       ',
+    Ans:"Any Personal Identifiable Information (PII) in the devices you sell will never be exposed to or shared with anyone for any purpose. As per our quality & security guidelines, our representative will do a mandatory full factory reset right infront of you during your scheduled item pickup. While it is not necessary, we'd also recommend you do a factory reset from your end to have a sigh of relief.",
+  },
+  {
+    Category:'General',
+    Ques: 'How is the price of my item calculated?',
+    Ans:"Zhiffy works with data-partners that provide a price quote for your device based on the information provided by you when placing your order. This assessment is based on the following criteria<br></br>1) The demand & supply of the item in the market.<br></br>2) The age of the device.<br></br>3) Whether it turns on or not, or is functional at all.<br></br>4) The status of its critical parts – screen, display, battery, speaker, camera and more.<br></br>5) The availability of a valid bill.<br></br>6) The availability of warranty.<br></br>Just like you, we hate price revisions and for that we insist our customers to provide accurate information of the device's condition to help us provide you the experience we'd like to deliver.",
+  },
+  {
+    Category:'General',
+    Ques: 'Can I cancel my order?   ',
+    Ans:"Check this <b>Table</b>",
+  },
+  {
+    Category:'General',
+    Ques: 'Why do you only give Second-hand electronics in exchange?',
+    Ans:" A New item is the best quality you can get today. But the unknown fact is, because of the <br></br>Our aim is provide a equal quality alternative to a New item. So the only difference is price. Our vendor partners are chosen specifically",
+  },
+  {
+    Category:'General',
+    Ques: 'What is the difference between a Refurbished product and a used one?',
+    Ans:"In both cases, the two devices are pre-owned. The similarities end there. A refurbished device has been tuned up and brought back to perfect working condition in a factory before being sold on Zhiffy (or somewhere else). So basically a refurbished phone has the double advantage of being a cheap smartphone while also having a professional refurbisher guarantee its excellent quality.",
+  },
+  {
+    Category:'General',
+    Ques: 'What criteria does a product have to meet before it can be sold to a customer?       ',
+    Ans:"We only have one criterion : The condition of the item has to be accurately described.<br></br>This helps us calculate and quote you the correct price. If we find that the item is not in the exact described condition, we'd be requoting you with a revised pricing mentioning the reason for the difference. If you agree to the revised pricing, we will proceed to pay you and process the exchange order further. If not, we will ship the item back to you as is free of charge.",
+  },
+  {
+    Category:'General',
+    Ques: 'Should I pick an item in exchange that is only of equal value?',
+    Ans:"The exchange item's value should be atleast 50% of the price you get. For eg: If you are quoted 10000 Rs. for the item you are selling, the item or items you are looking for exchange, should atleast have a total value of 5000 Rs.",
+  },
+  {
+    Category:'General',
+    Ques: 'Does Zhiffy accept water damaged phones?',
+    Ans:"We might have to take a call on that after taking a look. Our mission is to declutter the waste from your house and get something else that's valuable in return. So our primary objective would be that, be it a working piece or damaged.",
+  },
+  {
+    Category:'Payment',
+    Ques: 'How and when do I get paid?  ',
+    Ans:"After you select the item you want in exchange, we will pick up the item. You will receive the amount within 24 hours from the time we receive the item. ",
+  },
+  {
+    Category:'Payment',
+    Ques: 'What methods of payment do you support? ',
+    Ans:"We accept only Cash-on-Delivery & UPI based payments as of today. ",
+  }, 
+  {
+    Category:'Shipping',
+    Ques: ' Are contents insured against loss or damage?',
+    Ans:"We do not have an official insurance policy per se. However, if we do end up causing damange to your item anytime after it reaches us, you will never be at loss. Your exchange request will continue to hold the same value and importance to us.",
+  },  
+];
