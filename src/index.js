02b484fb8ba1531c821cce5edea8c3b8e3ecdae9
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2020-06-18 17:39:52 +0530

    changing index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import 'tachyons';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Routes from './Routes';
-import Admin from './Components/Admin';
-ReactDOM.render(<Admin />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
